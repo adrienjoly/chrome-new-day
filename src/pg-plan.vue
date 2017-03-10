65365230abb0cada233b08b05a3a15af54d2e7ab
Adrien Joly <adrien@whyd.com>
2017-03-10 18:18:59 +0100

    drag and drop tasks

--- src/pg-plan.vue
+++ src/pg-plan.vue
@@ -2,18 +2,24 @@
   <div class="pg-plan">
     <h1>Plan your day</h1>
     <ul>
-      <li v-for="task in tasks">
-        {{ task }}
-      </li>
+      <draggable v-model="tasks" @end="onDragEnd">
+        <li v-for="task in tasks">
+          {{ task }}
+        </li>
+      </draggable>
     </ul>
     <input @change="onAddTask">
   </div>
 </template>
 
 <script>
+  import draggable from 'vuedraggable'
   import db from './db.js'
 
   export default {
+    components: {
+      draggable,
+    },
     data: () => ({
       tasks: [],
     }),
@@ -24,6 +30,10 @@
       // TODO: db.unsubscribeToData('tasks')
     },
     methods: {
+      onDragEnd: function(evt) {
+        console.log('onDragEnd', evt)
+        db.setData('tasks', this.tasks, () => console.log('saved.'))
+      },
       onAddTask: function(evt) {
         const task = evt.target.value
         db.setData('tasks', this.tasks.concat([ task ]), () => console.log('added:', task))
