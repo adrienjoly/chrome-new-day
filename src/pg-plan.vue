cc3703b808f6c7de1ed84537631078c01f9ae180
Adrien Joly <adrien@whyd.com>
2017-03-10 18:30:27 +0100

    tasks CRUD

--- src/pg-plan.vue
+++ src/pg-plan.vue
@@ -1,10 +1,25 @@
+<style>
+  li .task-delete {
+    display: none;
+  }
+  li:hover .task-delete {
+    display: inline;
+    cursor: pointer;
+    color: gray;
+  }
+  li:hover .task-delete:hover {
+    color: black;
+  }
+</style>
+
 <template>
   <div class="pg-plan">
     <h1>Plan your day</h1>
     <ul>
       <draggable v-model="tasks" @end="onDragEnd">
-        <li v-for="task in tasks">
+        <li v-for="task in tasks" :key="task" :data-name="task">
           {{ task }}
+          <span class="task-delete" @click="onDeleteTask"> [delete]</span>
         </li>
       </draggable>
     </ul>
@@ -33,6 +48,11 @@
       onDragEnd: function(evt) {
         console.log('onDragEnd', evt)
         db.setData('tasks', this.tasks, () => console.log('saved.'))
+      },
+      onDeleteTask: function(evt) {
+        const delTaskName = evt.target.parentElement.getAttribute('data-name')
+        const tasks = this.tasks.filter((taskName) => taskName !== delTaskName)
+        db.setData('tasks', tasks, () => console.log('removed:', delTaskName))
       },
       onAddTask: function(evt) {
         const task = evt.target.value
