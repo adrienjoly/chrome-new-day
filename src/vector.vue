a5c5749487d9b43aa1e7463a02741e1e90ad765a
Jeremie Girault <jeremie.girault@gmail.com>
2017-03-17 15:38:50 +0100

    mood screen

---
+++ src/vector.vue
@@ -0,0 +1,18 @@
+<style>
+  .vector-container {
+  	height: 100%;
+  	width: 100%;
+  	display: inline-block;
+  }
+</style>
+
+<template>
+	<div class="vector-container" v-once v-html="src">
+	</div>
+</template>
+
+<script>
+  export default {
+    props: [ 'src' ]
+  }
+</script>
