2e40a3066845dce28ad74df5e72f7556c0e76016
Jeremie Girault <jeremie.girault@gmail.com>
2017-03-17 12:44:28 +0100

    mood screen

---
+++ src/pg-mood.vue
@@ -0,0 +1,20 @@
+<style>
+  
+</style>
+
+<template>
+  <div>
+    Mood
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [ 'db' ],
+    data: () => ({}),
+    mounted() { 
+    },
+    methods: {
+    }
+  }
+</script>
