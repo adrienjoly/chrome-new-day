d69189af323799f50c3cd32dcf78222129b7ea2b
Adrien Joly <adrien@whyd.com>
2017-03-17 17:07:26 +0100

    integration of jeremie's component, for https://trello.com/c/ZbhWHyuL/4-pg-focus-progression-en-haut-d-ecran

--- src/pg-focus.vue
+++ src/pg-focus.vue
@@ -34,13 +34,13 @@
 
 <template>
   <div class="pg-focus centered">
-    <div class="focus-notifs">
-      <notif-review />
-      <page-indicator :pages="pages" />
-    </div>
     <div class="centered">
       <h1>{{ task.name }}</h1>
       <button class="button btn-next" @click="onDone">It's done</button>
+    </div>
+    <div class="focus-notifs">
+      <notif-review />
+      <page-indicator :pages="tasks" @page-changed="goToTask" />
     </div>
     <div class="prev-task" @click="skipToPrev">&lt;</div>
     <div class="next-task" @click="skipToNext">&gt;</div>
@@ -52,7 +52,6 @@
   import PageIndicator from './ui-page-indicator.vue';
 
   // TODO: polish
-  // TODO: integrate task progress on top
   // TODO: integrate "Snooze" button on top-right corner
   export default {
     components: {
@@ -68,11 +67,7 @@
     data: () => ({
       taskindex: null,
       task: {},
-      pages: [
-        { title: "page1", done: true }, 
-        { title: "page2", current: true, done: true }, 
-        { title: "page3", } 
-      ]
+      tasks: [],
     }),
     watch: {
       // call again the method if the route changes
@@ -97,11 +92,19 @@
           this.$router.push('/focus/' + index)
         })
       },
+      goToTask(taskIndex) {
+        this.$router.push('/focus/' + taskIndex)        
+      },
       fetchData() {
         this.db.fetchData('tasks', ({ key, value }) => {
           const tasks = value || []
           // 1. update view data based on route parameter
           this.taskindex = parseInt(this.$route.params.taskindex)
+          this.tasks = tasks.map((task, i) => ({
+            title: task.name,
+            done: task.done,
+            current: i === this.taskindex,
+          }))
           // 2. update current task (based on route parameter)
           const now = new Date().getTime()
           this.task = Object.assign({}, tasks[this.taskindex], { lastStart: now })
